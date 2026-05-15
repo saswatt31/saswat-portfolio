83f1b0e4465e3c4d51cb6e45ba44540fe5999733
Saswat Pattanaik <saswatpattanaik31@gmail.com>
2026-05-15 19:40:59 +0530

    update-4

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -167,7 +167,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "CodeSync.png",
+      image: "/CodeSync.png",
       video: "",
     },
     {
@@ -197,7 +197,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "StreetScan.png",
+      image: "/StreetScan.png",
       video: "",
     },
     {
@@ -226,7 +226,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "FitMindAI.png",
+      image: "/FitMindAI.png",
       video: "",
     },
     {
